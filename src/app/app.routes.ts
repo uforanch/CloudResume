549c2770e23e494d3003056f3d3dc0b3cdcadabc
uforanch <incenate@gmail.com>
2025-08-06 15:42:05 -0400

    new route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {Experience } from './components/experience/experience'
 import {Portfolio } from './components/portfolio/portfolio'
 import { Education } from './components/education/education';
+import { Welcome } from './components/welcome/welcome';
 export const routes: Routes = [
+    {path:"welcome", component:Welcome},
     {path:"education", component:Education},
     {path:"experience", component:Experience},
     {path:"portfolio", component:Portfolio},
-    {path:"", redirectTo:"/education", pathMatch:"full"}
+    {path:"", redirectTo:"/welcome", pathMatch:"full"}
 ];
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
   export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000' // Your development API URL
+    apiUrl: 'https://e7yi5oxlgi.execute-api.us-east-1.amazonaws.com/' // Your development API URL
   };
